c01b5476533af18b4bb6a97b1f20359c4f6ddd3f
Devimrln <79152123+Devimrln@users.noreply.github.com>
2021-02-23 17:16:03 +0900

    Add files via upload

---
+++ LAB-3-StrukturKontrolPercabangan.py
@@ -0,0 +1,21 @@
+#Devi Marline Muhammad (71180366)
+
+'''
+Buatlah sebuah program yang dapat mengecek  
+bilangan ganjil dan genap. program meminta pengguna memasukkan
+angka yang akan di cek, apakah bilangan tersebut bilangan ganjil atau
+bilangan genap
+'''
+
+#input
+bilangan = int(input("Masukkan bilangan :"))
+#proses
+if (bilangan % 2 != 0):
+    #output
+    print("%d adalah Bilangan Ganjil" % bilangan)
+else:
+    print("%d adalah Bilangan Genap" % bilangan)
+    
+
+        
+
